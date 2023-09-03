95163670ec812e0eb08b8a69babe2e33658e5d45
ndegz <ndegwa8781@gmail.com>
2023-09-03 11:12:05 +0300

    added more fix

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -6,7 +6,7 @@ main file
 filter_datum = __import__('filtered_logger').filter_datum
 
 fields  = ["password", "date_of_birth"]
-messages = ["name=egg;email=egg@eggssample.com;password=eggcelent;date_of_birth=12/12/1996;", "name=bob;email=bob@dylan.com;password=bobycool;date_of_birth=03/04/1993;"]
+messages = ["name=egg;email=eggmin@eggssample.com;password=eggcellent;date_of_birth=12/12/1996;", "name=bob;email=bob@dylan.com;password=bobycool;date_of_birth=03/04/1993;"]
 
 for message in messages:
     print(filter_datum(fields, 'xxx', message, ';'))
