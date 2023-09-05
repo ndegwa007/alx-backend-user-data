08ffc7dce9668692a7e6e788b8886d99987b2ea9
ndegz <ndegwa8781@gmail.com>
2023-09-05 21:39:57 +0300

    bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,13 +3,13 @@
 import bcrypt
 
 
-def hash_password(password: str) -> hash:
+def hash_password(password: str) -> bytes:
     """use bycrpt to encrypt"""
     byt = password.encode('UTF-8')
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(byt, salt)
     return hashed
-
+    
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """check password is legit"""
