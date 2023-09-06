2f9bbe6bbbe47c931b941a57af7cc3f5d6f074ca
ndegz <ndegwa8781@gmail.com>
2023-09-06 21:21:49 +0300

    flask basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,18 +14,17 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-
-@app.errorhandler(401)
-def unauthorized(e) -> str:
-    """unauthorized"""
-    return jsonify(e), 401
-
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """unauthorized"""
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
-    return abort(401)
+    """return unauthorized"""
+    abort(401)
