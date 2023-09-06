bd0885fcfa4cdf5c20baccd1dae856d9801c4b9e
ndegz <ndegwa8781@gmail.com>
2023-09-06 21:58:22 +0300

    basic auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized():
     """return unauthorized"""
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """forbidden route"""
+    abort(403)
